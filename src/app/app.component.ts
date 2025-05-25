05231280752252231a1f11000accedb737825c9f
ankur-vasta <ankur.vasta@gmail.com>
2025-05-25 10:17:34 +0530

    Tailwind and Prime Ng setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'devconnect-frontend';
+
+  onClick() {
+    console.log('Button clicked!');
+  }
 }
